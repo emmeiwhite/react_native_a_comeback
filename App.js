f63b29b11d7ffda9c8c054f1edc277f9ec7731eb
emmeiwhite <emmeiwhite@gmail.com>
2018-12-10 14:56:33 +0530

    Working with default components

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,TextInput} from 'react-native';
+import {Platform, StyleSheet, Text, View,TextInput,Alert,Button,TouchableHighlight} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -26,20 +26,36 @@ export default class App extends Component {
     }
   }
 
+  callingPankaj = (e)=>{
+    Alert.alert(this.state.userName);
+  }
+  
+  _onPressButton = (e)=>{
+    Alert.alert("THIS IS TOUCHABLE HIGHLIGHT")
+  }
   render() {
     return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text>HELLO EMMEI</Text>
+        <Text style={styles.headerText}>HELLO EMMEI</Text>
       </View>
       <View style={styles.main}>
       <TextInput
-          style={{height: 40}}
+          style={styles.userInput}
           placeholder="Type here to translate!"
           onChangeText={(text) => this.setState({userName:text})}
         />
       
+        <Button onPress={this.callingPankaj} title="CLICK ME"></Button>
+        
+        <TouchableHighlight onPress={this._onPressButton}>
+        <Image
+        style={styles.button}
+        source={require('image!myButton')}
+      />
+        </TouchableHighlight>
       </View>
+
       <View style={styles.footer}>
         <Text>
            UserName State is :{this.state.userName}
@@ -52,27 +68,42 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   header:{
     flex:1,
-    backgroundColor:'#990099',
+    backgroundColor:'#90000e',
     alignItems:'center',
     justifyContent:'center',
+    color:'#ffffff'
+  },
+  headerText:{
+    color:'#ffffff',
     fontSize:30
   },
   main:{
-    flex:4,
-    backgroundColor:'#337733',
+    flex:8,
+    backgroundColor:'teal',
     alignItems:'center',
-    justifyContent:'center'
+    // justifyContent:'center',
+    fontSize:18
   },
   footer:{
     flex:1,
-    backgroundColor:'#0000FF',
+    backgroundColor:'#90000e',
     color:'#FFFFFF',
     alignItems:'center',
     justifyContent:'center'
 
+  },
+  userInput:{
+    borderWidth:1,
+    borderColor:'gray',
+    // paddingTop:15,
+    // paddingBottom:15,
+    // paddingLeft:15,
+    // paddingRight:15,
+    padding:15,
+    marginBottom:20
   }
 });
